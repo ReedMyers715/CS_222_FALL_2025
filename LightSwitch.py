2abddf9fd77a3bfeb04a1928135492294a119b73
ReedMyers715 <reed.myers@bsu.edu>
2025-09-11 16:07:30 +0000

    Oobject oriented

---
+++ LightSwitch.py
@@ -0,0 +1,16 @@
+class LightSwitch():
+    def __init__(self):
+        self.switchIsOn = False
+        def turnOn(self):
+            self.switchIsOn = True
+        def turnOff(self):
+            self.switchIsOn = False
+
+def Main():
+    l0 = LightSwitch()
+    print(l0.switchIsOn)
+    l0.turnOn()
+    print(l0.switchIsOn)
+    l0.turnOff()
+    print(l0.switchIsOn)
+Main()
