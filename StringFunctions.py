d62695b4afbcea0762cc90bb95ac0dfbc68ead60
ReedMyers715 <reed.myers@bsu.edu>
2025-09-18 15:28:11 +0000

    Added string functions

---
+++ StringFunctions.py
@@ -0,0 +1,13 @@
+def main():
+    bsu = "Ball State University"
+    print(bsu[2])
+    #bsu[2] = 'x'
+    print(bsu[:5])
+    print(bsu.startswith("Ball"))
+    print(bsu.upper())
+    print(bsu.isalnum())
+    print(bsu.find("t"))
+    print(bsu.rfind("t"))
+    print(bsu.replace("a","x"))
+
+main()
